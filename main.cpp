b4b4bdb2890df83757422c370519c788ece1d960
leeseungwon <free_sstyle@naver.com>
2022-04-27 16:50:39 +0900

    random

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	int Money = 0; // 변수를 선언했으면 일단 초기화 시켜놔라
+	Money = 100;
+	srand(time(nullptr));
 
+	cout << rand() % 3 << endl;
+	
 	return 0;
 }
